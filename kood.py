ef9eab0407049d40d8a94557540da9e3686c9c3a
marleneibrus <marlene.ibrus@gmail.com>
2022-11-07 18:35:32 +0200

    setup

--- kood.py
+++ kood.py
@@ -1,1 +1,16 @@
 #koodifail
+import pygame
+import random
+import time
+
+
+pygame.init()
+
+(laius, suurus) = (300, 200)
+ekraan = pygame.display.set_mode((laius, suurus))
+
+programm_käib = True
+while programm_käib:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            programm_käib = False
