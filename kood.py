10a007dfa4cb3390ee3c1c8c2ba8429a07ba90a5
marleneibrus <marlene.ibrus@gmail.com>
2022-11-09 18:30:16 +0200

    panin piirid ära, veitsa lappes on tho

--- kood.py
+++ kood.py
@@ -1,18 +1,19 @@
-# Tegelasele ja marjale peaks assignima class'id ja paika panema mänguvälja piirid, hetkel saab karu ekraanist välja minna
 # Koobas võiks ka ss class'iga olla, saab collisioniga määrata?
 
 # ==== IMPORTIMINE ====
 import pygame
 import random
+from pygame.locals import *
 import time
 import operator
 
 # ==== SETUP JA TAUSTA VÄRV ====
 pygame.init()
 
-(laius, suurus) = (0,0)
-ekraan = pygame.display.set_mode(size = (laius, suurus))
+(laius, suurus) = (1000,800)
+ekraan = pygame.display.set_mode((laius, suurus))
 pygame.display.set_caption('Nefi matemaatikamäng :3')
+fullscreen = False
 
 taustavärv = (50,90,10)
 
@@ -21,9 +22,12 @@ taustavärv = (50,90,10)
 # Meie karu nimi on Nefi
 nefi_pilt = pygame.image.load("Nefi_seisab.png").convert_alpha()
 nefi_pilt = pygame.transform.scale(nefi_pilt, (250, 200))
+rect = nefi_pilt.get_rect()
+print(rect)
+nefi_asukoht = pygame.Rect(0, 0, 250, 200)
 
-def lisa_nefi_asukohal(x,y):
-    ekraan.blit(nefi_pilt, (x,y))
+def lisa_nefi_asukohal(nefi_pilt,rect):
+    ekraan.blit(nefi_pilt, rect)
 
 x = 500
 y = 300
@@ -62,7 +66,7 @@ def arvutamismäng():
 base_font = pygame.font.Font(None,32)
 user_tekst = " "
 tehe = suvaline_avaldis()
-koopa_asukoht = pygame.Rect(1000,600,200,200)
+koopa_asukoht = pygame.Rect(750,675,100,100)
 koopa_värv = pygame.Color("black")
 klikk_kasti = False
 
@@ -90,26 +94,26 @@ while programm_käib:
                 y_muutus = -2
             elif event.key == pygame.K_DOWN:
                 y_muutus = 2
-            
+
             #lubab kirjutada/kustutada kui kastile on klikitud
             if klikk_kasti == True:
                 if event.key == pygame.K_BACKSPACE:
                     user_tekst = user_tekst[:-1]
                 else:
                     user_tekst += event.unicode
+        if x > laius or x < 0:
+            x_muutus = 0
         
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT or event.key == pygame.K_UP or event.key == pygame.K_DOWN:
                 x_muutus = 0
                 y_muutus = 0
-        
-    x += x_muutus
-    y += y_muutus
-
-
+    
+    ekraan.blit(nefi_pilt, nefi_asukoht)
+    nefi_asukoht.move_ip(x_muutus, y_muutus)
+    nefi_asukoht.clamp_ip(ekraan.get_rect())
     ekraan.fill(taustavärv)
-    lisa_nefi_asukohal(x,y)
-
+    pygame.display.flip()
 # tekitab koopa kasti, prindib tehte, koht kuhu saab kirjutada
     pygame.draw.rect(ekraan,koopa_värv,koopa_asukoht,2)
     tekstipind = base_font.render(user_tekst, True, (255,255,255))
@@ -118,5 +122,7 @@ while programm_käib:
     avaldise_kuvamine = base_font.render(tehe, True, (255,255,255))
     ekraan.blit(avaldise_kuvamine,(koopa_asukoht.x + 5, koopa_asukoht.y + 5))
 
+    pygame.display.flip()
 
-    pygame.display.update()
+pygame.quit()
+    #pygame.display.update()
