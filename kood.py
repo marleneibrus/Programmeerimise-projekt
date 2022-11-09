2cecdf3b068a8468bfa02baea8c0b82ce8995c32
marleneibrus <marlene.ibrus@gmail.com>
2022-11-09 20:43:55 +0200

    taimer

--- kood.py
+++ kood.py
@@ -1,21 +1,22 @@
-# Koobas võiks ka ss class'iga olla, saab collisioniga määrata?
 
 # ==== IMPORTIMINE ====
 import pygame
 import random
 from pygame.locals import *
-import time
+import time, datetime
 import operator
+
 
 # ==== SETUP JA TAUSTA VÄRV ====
 pygame.init()
 
-(laius, suurus) = (1000,800)
-ekraan = pygame.display.set_mode((laius, suurus))
+(kõrgus, laius) = (1000,800)
+ekraan = pygame.display.set_mode((kõrgus, laius))
 pygame.display.set_caption('Nefi matemaatikamäng :3')
 fullscreen = False
 
 taustavärv = (50,90,10)
+valge = (255,255,255)
 
 
 # ==== TEGELANE ====
@@ -25,16 +26,35 @@ nefi_pilt = pygame.transform.scale(nefi_pilt, (80, 100))
 rect1 = nefi_pilt.get_rect()
 nefi_asukoht = pygame.Rect(0, 0, 80, 100)
 
+x = 0
+y = 0
+x_muutus = 0
+y_muutus = 0
+
+
+# ==== MARJA GENEREERIMINE JA ÜLES KORJAMINE ====
 mari_pilt = pygame.image.load('mari.xcf').convert_alpha()
 mari_pilt = pygame.transform.scale(mari_pilt, (35, 35))
 rect2 = mari_pilt.get_rect()
 mari_asukoht = pygame.Rect(0, 0, 35, 35)
 
 
-x = 0
-y = 0
-x_muutus = 0
-y_muutus = 0
+# ==== TAIMER JA LÕPUEKRAAN ====
+taimer = datetime.datetime.utcnow() + datetime.timedelta(seconds=10)
+
+def sõnum_ekraanile(tekst, font):
+    tekstsurface = font.render(tekst, True, (255,255,255))
+    return tekstsurface, tekstsurface.get_rect()
+
+def sõnum(sõnum):
+    tekst_vorm = pygame.font.Font(None, 100)
+    tekstsurf, tekstrect = sõnum_ekraanile(sõnum, tekst_vorm)
+    tekstrect.keskpaik = ((kõrgus / 2), (laius / 2))
+
+    ekraan.blit(tekstsurf, tekstrect)
+    pygame.display.flip()
+    time.sleep(10)
+    
 
 # ==== KOOPA EHITAMISE JAOKS MÕELDUD ARVUTAMISMÄNG ====
 # funktsioon genereerib suvalise avaldise 
@@ -63,6 +83,7 @@ def arvutamismäng():
             punktid -= 10
             print("vale vastus!")
 
+
 # ==== KOOBAS ====
 base_font = pygame.font.Font(None,32)
 user_tekst = " "
@@ -72,59 +93,77 @@ koopa_värv = pygame.Color("black")
 klikk_kasti = False
 
 
+# ==== PUNKTID ====
+def punktisumma(punktid):
+    punkti_font = pygame.font.SysFont(None, 32)
+    punktid_ekr = punkti_font.render('Punktid: '+str(punktid), True, (255,255,255))
+    ekraan.blit(punktid_ekr, (0,0))
+
+skoor = 0
+
+
 # ==== MÄNG ====
 programm_käib = True
+mängu_aeg = True
 
 while programm_käib:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            programm_käib = False
+    while mängu_aeg:
 
-        if event.type == pygame.MOUSEBUTTONDOWN:
-                    if koopa_asukoht.collidepoint(event.pos):
-                        klikk_kasti = True
-                    else:
-                        klikk_kasti = False
-
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                x_muutus = -2
-            elif event.key == pygame.K_RIGHT:
-                x_muutus = 2
-            elif event.key == pygame.K_UP:
-                y_muutus = -2
-            elif event.key == pygame.K_DOWN:
-                y_muutus = 2
-
-            #lubab kirjutada/kustutada kui kastile on klikitud
-            if klikk_kasti == True:
-                if event.key == pygame.K_BACKSPACE:
-                    user_tekst = user_tekst[:-1]
-                else:
-                    user_tekst += event.unicode
-        if x > laius or x < 0:
-            x_muutus = 0
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                programm_käib = False
         
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT or event.key == pygame.K_UP or event.key == pygame.K_DOWN:
-                x_muutus = 0
-                y_muutus = 0
+            if datetime.datetime.utcnow() > taimer:
+                mängu_aeg = False
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                        if koopa_asukoht.collidepoint(event.pos):
+                            klikk_kasti = True
+                        else:
+                            klikk_kasti = False
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x_muutus = -2
+                elif event.key == pygame.K_RIGHT:
+                    x_muutus = 2
+                elif event.key == pygame.K_UP:
+                    y_muutus = -2
+                elif event.key == pygame.K_DOWN:
+                    y_muutus = 2
+
+                #lubab kirjutada/kustutada kui kastile on klikitud
+                if klikk_kasti == True:
+                    if event.key == pygame.K_BACKSPACE:
+                        user_tekst = user_tekst[:-1]
+                    else:
+                        user_tekst += event.unicode
+        
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT or event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                    x_muutus = 0
+                    y_muutus = 0
+
+        punktisumma(skoor)
+        ekraan.blit(nefi_pilt, nefi_asukoht)
+        ekraan.blit(mari_pilt, (100,100))
+        nefi_asukoht.move_ip(x_muutus, y_muutus)
+        nefi_asukoht.clamp_ip(ekraan.get_rect())
+        pygame.display.flip()
+
+        ekraan.fill(taustavärv)
+
+    # tekitab koopa kasti, prindib tehte, koht kuhu saab kirjutada
+        pygame.draw.rect(ekraan,koopa_värv,koopa_asukoht,2)
+        tekstipind = base_font.render(user_tekst, True, (255,255,255))
+        ekraan.blit(tekstipind,(koopa_asukoht.x + 5, koopa_asukoht.y + 50))
+        koopa_asukoht.w = max(200, tekstipind.get_width() + 10)
+        avaldise_kuvamine = base_font.render(tehe, True, (255,255,255))
+        ekraan.blit(avaldise_kuvamine,(koopa_asukoht.x + 5, koopa_asukoht.y + 5))
     
-    ekraan.blit(nefi_pilt, nefi_asukoht)
-    ekraan.blit(mari_pilt, mari_asukoht)
-    nefi_asukoht.move_ip(x_muutus, y_muutus)
-    nefi_asukoht.clamp_ip(ekraan.get_rect())
-
-    pygame.display.flip()
-
-    ekraan.fill(taustavärv)
-# tekitab koopa kasti, prindib tehte, koht kuhu saab kirjutada
-    pygame.draw.rect(ekraan,koopa_värv,koopa_asukoht,2)
-    tekstipind = base_font.render(user_tekst, True, (255,255,255))
-    ekraan.blit(tekstipind,(koopa_asukoht.x + 5, koopa_asukoht.y + 50))
-    koopa_asukoht.w = max(200, tekstipind.get_width() + 10)
-    avaldise_kuvamine = base_font.render(tehe, True, (255,255,255))
-    ekraan.blit(avaldise_kuvamine,(koopa_asukoht.x + 5, koopa_asukoht.y + 5))
+    
+    sõnum('Mäng läbi!')
+    time.sleep(10)
 
 
 
